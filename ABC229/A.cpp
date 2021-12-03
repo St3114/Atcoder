ee659e4f9d825c9989ee94e36d116287a885957b
St3114 <s2854091@gmail.com>
2021-12-03 17:35:24 +0900

    沼った

---
+++ ABC229/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  if(s1 == ".#" && s2 == "#."){
+    cout << "No" << endl;
+  }else if(s1 == "#." && s2 == ".#"){
+    cout << "No" << endl;
+  }else{
+    cout << "Yes" << endl;
+  }
+  return 0;
+} 
